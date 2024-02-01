b38f9ff1ef2d5a598c5392089249c4b938046224
Oskari Järvinen <90318358+oskarija@users.noreply.github.com>
2024-02-01 22:29:19 +0200

    Contact update

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function copyPhoneNumber() {
     navigator.clipboard.writeText
         ("+35850505758178");
 }
+
+function copyEmail() {
+    
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("oskari.jarvinen@gmail.com")
+}
