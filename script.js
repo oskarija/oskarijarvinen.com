3b642629c4fa2a25e7cb67a3af8652403b86278a
Oskari Järvinen <90318358+oskarija@users.noreply.github.com>
2024-02-02 16:51:49 +0200

    Contact copy to clipboard

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function copyPhoneNumber() {
-
     /* Copy text into clipboard */
-    navigator.clipboard.writeText
-        ("+35850505758178");
+    navigator.clipboard.writeText("+35850505758178").then(function() {
+        showFeedback();
+    }).catch(function(error) {
+        console.error('Failed to copy:', error);
+    });
 }
 
 function copyEmail() {
-    
     /* Copy text into clipboard */
-    navigator.clipboard.writeText
-        ("oskari.jarvinen@gmail.com")
+    navigator.clipboard.writeText("oskari.jarvinen@gmail.com").then(function() {
+        showFeedback();
+    }).catch(function(error) {
+        console.error('Failed to copy:', error);
+    });
+}
+
+function showFeedback() {
+    const feedback = document.getElementById('feedback');
+    feedback.textContent = 'Copied to clipboard';
+    feedback.classList.add('show');
+
+    setTimeout(() => {
+        feedback.classList.remove('show');
+    }, 2000); // Adjust the time for how long you want the feedback to be displayed (in milliseconds)
 }
